6cdb03c70f84454462ceb075505a4492201bb121
Eamin Zhang <eamin.zhang@srbtech.com>
2015-02-26 12:24:30 -0500

    Updated SDK MQTT & STOMP Java

---
+++ sdk/MQTT/Java/Consumer.java
@@ -0,0 +1,124 @@
+/**
+ * File: Consumer.java
+ * Description: This is the MQTT consumer handles incoming messages
+ *     published by producers from a particular topic.
+ *     Consumer prints the topic and payload as it receives messages.
+ *
+ * Author: Eamin Zhang
+ * robomq.io (http://www.robomq.io)
+ */
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+
+public class Consumer {
+
+	private MqttClient client;
+	private String server = "hostname";
+	private String port = "1883";
+	private String broker = "tcp://" + server + ":" + port;
+	private String vhost = "yourvhost";
+	private String username = "username";
+	private String password = "password";
+	private String topic = "test/#";
+	private String clientId = MqttClient.generateClientId();
+	private MemoryPersistence persistence = new MemoryPersistence();
+
+	/**
+	 * This method is the overrided callback on receiving messages.
+	 * @ It is event-driven. You don't call it in your code.
+	 * @ It prints the message topic and payload on console.
+	 * @ There're other callback functions provided by this library.
+	 */
+	private class onMessage implements MqttCallback {
+	
+		public void messageArrived(String topic, MqttMessage message) {		
+			System.out.println("Topic: " + topic + ", Message: " + (new String(message.getPayload())));
+		}
+
+		public void connectionLost(Throwable cause) {
+
+		}
+
+		public void deliveryComplete(IMqttDeliveryToken token) {
+
+		}
+	}
+
+	/**
+	 * This method connects client to the broker.
+	 * @ exception on connection error.
+	 */
+	private void connect() {
+		try {
+			client = new MqttClient(broker, clientId, persistence);
+			MqttConnectOptions connOpts = new MqttConnectOptions();
+			connOpts.setUserName(vhost + ":" + username);
+			connOpts.setPassword(password.toCharArray());
+			connOpts.setKeepAliveInterval(60);
+			connOpts.setCleanSession(true);
+			client.connect(connOpts);
+		} catch(MqttException me) {
+			System.out.println("Error: "+me);
+			System.exit(-1);
+		}
+	}
+
+	/**
+	 * This method sucscribes the topic.
+	 * @ exception on subscription error.
+	 */
+	private void subscribe() {
+		try {
+			onMessage callback = new onMessage();
+			client.setCallback(callback);
+			client.subscribe(topic, 1);	//qos=1
+		} catch(MqttException me) {
+			System.out.println("Error: "+me);
+			System.exit(-1);		
+		}
+	}
+
+	/**
+	 * This method unsucscribes the topic.
+	 * @ exception on unsubscription error.
+	 */
+	private void unsubscribe() {
+		try {
+			client.unsubscribe(topic);
+		} catch(MqttException me) {
+			System.out.println("Error: "+me);
+			System.exit(-1);			
+		}
+	}
+
+	/**
+	 * This method disconnect client from the broker.
+	 * @ exception on disconnection error.
+	 */
+	private void disconnect() {
+		try {
+			client.disconnect();
+			System.exit(0);
+		} catch(MqttException me) {
+			System.out.println("Error: "+me);
+			System.exit(-1);			
+		}
+	}
+
+	/**
+	 * This is the main method which creates and runs consumer instance.
+	*/
+	public static void main(String[] args) {
+		Consumer c = new Consumer();
+		c.connect();
+		c.subscribe();
+		//c.unsubscribe();
+		//c.disconnect();
+	}
+}
---
+++ sdk/STOMP/Java/Consumer.java
@@ -0,0 +1,95 @@
+/**
+ * File: Consumer.java
+ * Description: This is the STOMP consumer handles incoming messages
+ *     published by producers from a particular queue.
+ *     Consumer prints the message body as it receives messages.
+ *
+ * Author: Eamin Zhang
+ * robomq.io (http://www.robomq.io)
+ */
+
+import net.ser1.stomp.*;
+import java.util.Map;
+
+class Consumer {
+	private Client client;
+	private String server = "hostname";
+	private int port = 61613;
+	private String vhost = "yourvhost";
+	private String destination = "/queue/test";	//There're more options other than /queue/...
+	private String login = "username";
+	private String passcode = "password";
+
+	/**
+	 * This method connects client to the broker.
+	 * @ exception on connection error.
+	 */
+	private void connect() {
+		try {
+			client = new Client(server, port, login, passcode, vhost);
+		} catch(Exception e) {
+			System.out.println("Error: Can't initialize connection");
+			System.exit(-1);
+		}		
+	}
+
+	/**
+	 * This method sucscribes the destination.
+	 * @ exception on subscription error.
+	 */
+	private void subscribe() {
+		try {
+			client.subscribe(destination, new Listener() {
+				/**
+				 * This method is the overrided callback on receiving messages.
+				 * @ It is event-driven. You don't call it in your code.
+				 * @ It prints the message body on console.
+				 * @ There're other callback functions provided by this library.
+				 */
+				public void message( Map headers, String body ) {
+					System.out.println(body);
+				}
+  			}); 
+		} catch(Exception e) {
+			System.out.println("Error: Can't subscribe queue");
+			System.exit(-1);		
+		}	
+	}
+
+	/**
+	 * This method unsucscribes the destination.
+	 * @ exception on unsubscription error.
+	 */
+	private void unsubscribe() {
+		try {
+			client.unsubscribe(destination);
+		} catch(Exception e) {
+			System.out.println("Error: Can't unsubscribe");
+			System.exit(-1);			
+		}
+	}
+
+	/**
+	 * This method disconnect client from the broker.
+	 * @ exception on disconnection error.
+	 */
+	private void disconnect() {
+		try {
+			client.disconnect();
+		} catch(Exception e) {
+			System.out.println("Error: Can't disconnect");
+			System.exit(-1);			
+		}
+	}
+
+	/**
+	 * This is the main method which creates and runs consumer instance.
+	*/
+	public static void main(String[] args) {
+		Consumer c = new Consumer();
+		c.connect();
+		c.subscribe();
+		//c.unsubscribe();
+		//c.disconnect();
+	}
+}
